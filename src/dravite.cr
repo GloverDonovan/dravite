64aff44683099f86285cf4b533d22873171fb9ce
Donovan Glover <donovan@dglover.co>
2018-02-09 23:58:25 -0500

    Various additions to dravite.cr

--- src/dravite.cr
+++ src/dravite.cr
@@ -5,4 +5,42 @@ require "colorize"
 require "option_parser"
 
 module Dravite
+  extend self
+
+  VERSION = "0.1.0"
+
+  MARKD = Mard::Options.new(smart: true)
+
+  IN = {
+    :js       => "app",
+    :css      => "style",
+    :pages    => "pages",
+    :posts    => "posts",
+    :layouts  => "layouts",
+    :includes => "includes",
+    :data     => "data",
+  }
+
+  OUT = {
+    :js  => "app.js",
+    :css => "design.css",
+  }
+end
+
+OptionParser.parse! do |opts|
+  opts.banner = "Usage: drav [directory]"
+
+  opts.on("-h", "--help", "Show this help message") do
+    puts opts
+  end
+
+  opts.on("-v", "--version", "Show version information") do
+    puts Dravite::VERSION
+  end
+
+  opts.unknown_args do |args|
+    if args.size > 0
+      puts "The drav [directory] functionality is incomplete for now."
+    end
+  end
 end
